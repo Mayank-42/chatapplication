c16d96295fa64dc0143e6dfb3f938d581680a798
Mayank Raj <mayank.rj2004@gmail.com>
2026-08-24 10:59:02 -0700

    .

--- app/src/main/java/com/example/chatapplication/Data/Repo/convoInfoRepo.kt
+++ app/src/main/java/com/example/chatapplication/Data/Repo/convoInfoRepo.kt
@@ -2,13 +2,19 @@ package com.example.chatapplication.Data.Repo
 
 import com.example.chatapplication.Data.DAO.conversationId
 import com.example.chatapplication.Data.local.tables.CinversationId
+import com.example.chatapplication.Data.network.ApiService
 
 class convoInfoRepo(
-    private var work: conversationId
+    private var work: conversationId,
+    private var api: ApiService
 ) {
     var getingConvoInfo=work.getAllConvoInfo()
     suspend fun insertConvoInfo(info: CinversationId){
         work.putingInfo(info)
+    }
+
+    suspend fun putingInfoLocaly(info: CinversationId){
+        api.getConvoInfo()
     }
 
 }
